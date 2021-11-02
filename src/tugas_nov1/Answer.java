29100ba22ef13b53b34351f24bb5653119563ce7
Richie-Z <zakariarichie@yahoo.com>
2021-11-02 15:21:41 +0700

    answer for tugas nov 1

--- src/tugas_nov1/Answer.java
+++ src/tugas_nov1/Answer.java
@@ -1,9 +1,27 @@
 package tugas_nov1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Richie-PC
  */
 public class Answer {
-    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukan nilai awal : ");
+        int nilaiAwal = sc.nextInt();
+        System.out.print("Masukan nilai akhir : ");
+        int nilaiAkhir = sc.nextInt();
+        if (nilaiAwal > nilaiAkhir) {
+            for (int i = nilaiAwal; i >= nilaiAkhir; i--) {
+                System.out.println(i);
+            }
+            return;
+        }
+        for (int i = nilaiAwal; i <= nilaiAkhir; i++) {
+            System.out.println(i);
+        }
+    }
 }
